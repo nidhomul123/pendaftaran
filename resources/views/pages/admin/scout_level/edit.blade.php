c6f0538b6428bcf3cb4c8b3a61c1797d1346e1f1
DESKTOP-2ANV0GE\Fanky <andryfanky3@gmail.com>
2023-06-03 00:47:50 +0700

    feat: add master scout_level menu

---
+++ resources/views/pages/admin/scout_level/edit.blade.php
@@ -0,0 +1,121 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Edit Master Tingkatan Pramuka</h1>
+        {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+            class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> --}}
+    </div>
+
+    <a href="{{ route('master.scout_level') }}" class="btn btn-secondary btn-icon-split btn-sm mb-4">
+        <span class="icon text-white-50">
+            <i class="fas fa-arrow-left"></i>
+        </span>
+        <span class="text">Kembali</span>
+    </a>
+
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Data Master Tingkatan Pramuka</h6>
+        </div>
+        <div class="card-body">
+            <form id="scout_level_form" method="post">
+                @csrf
+                {{ method_field('PUT') }}
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group mb-3">
+                            <label class="mb-2">Tingkatan Pramuka <span class="text-danger">*</span></label>
+                            <input class="form-control" id="scout_level" name="scout_level" type="text" required
+                            value="{{ $data->scout_level }}"
+                            oninvalid="this.setCustomValidity('Tingkatan Pramuka tidak boleh kosong')"
+                            oninput="this.setCustomValidity('')" />
+                        </div>
+                    </div>
+                </div>
+                <button class="btn btn-success btn-icon-split btn-sm float-right">
+                    <span class="icon text-white-50">
+                        <i class="fas fa-save"></i>
+                    </span>
+                    <span class="text">Simpan</span>
+                </button>
+            </form>
+        </div>
+    </div>
+
+    <script src="{{ asset('js/sweetalert2@11.js') }}"></script>
+
+    <script type="text/javascript">
+        $("#scout_level_form").on("submit", function(event) {
+            Swal.fire({
+                title: 'Simpan Perubahan',
+                text: "Apakah anda yakin?",
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Simpan',
+                cancelButtonText: 'Batal',
+                customClass: {
+                    confirmButton: 'mr-2',
+                    cancelButton: 'ml-2'
+                }
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: 'Sedang Menyimpan Data!',
+                        html: 'Mohon menunggu',
+                        timerProgressBar: true,
+                        allowOutsideClick: false,
+                        allowEscapeKey: false,
+                        didOpen: () => {
+                            Swal.showLoading()
+                        }
+                    })
+
+                    $.ajax({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        url: "{{ route('master.scout_level.update', $data->id) }}",
+                        type: "POST",
+                        data: new FormData($('#scout_level_form')[0]),
+                        processData: false,
+                        contentType: false,
+                        success: function (res) {
+                            console.log(res);
+
+                            Swal.close()
+
+                            if (res.status) {
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: 'Sukses',
+                                    text: res.message,
+                                    allowOutsideClick: false,
+                                    allowEscapeKey: false
+                                }).then((result) => {
+                                    window.open("{{ route('master.scout_level') }}", '_self');
+                                })
+                            } else {
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Maaf',
+                                    text: res.message,
+                                    allowOutsideClick: false,
+                                    allowEscapeKey: false
+                                })
+                            }
+                        }
+                    });
+                }
+            })
+            event.preventDefault();
+        });
+    </script>
+
+</div>
+@endsection
---
+++ resources/views/pages/admin/scout_level/scout_level.blade.php
@@ -0,0 +1,139 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Master Tingkatan Pramuka</h1>
+        {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+            class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> --}}
+        <a href="{{ route('master.scout_level.create') }}" class="d-none d-sm-inline-block btn btn-primary shadow-sm btn-icon-split btn-sm">
+            <span class="icon text-white-50">
+                <i class="fas fa-plus"></i>
+            </span>
+            <span class="text">Tambah Data</span>
+        </a>
+    </div>
+
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Data Master Tingkatan Pramuka</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th width="30">No</th>
+                            <th>Tingkatan Pramuka</th>
+                            <th width="180"></th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <th width="30">No</th>
+                            <th>Tingkatan Pramuka</th>
+                            <th width="180"></th>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        @php
+                            $no = 1;
+                        @endphp
+                        @foreach ($data as $item)
+                            <tr>
+                                <td>{{ $no }}</td>
+                                <td>{{ $item->scout_level }}</td>
+                                <td class="text-center">
+                                    <a href="{{ route('master.scout_level.edit', $item->id) }}" class="btn btn-primary btn-icon-split btn-sm mr-1">
+                                        <span class="icon text-white-50">
+                                            <i class="fas fa-edit"></i>
+                                        </span>
+                                        <span class="text">Ubah</span>
+                                    </a>
+                                    <button class="btn btn-danger btn-icon-split btn-sm btn-destroy" data-id="{{ $item->id }}">
+                                        <span class="icon text-white-50">
+                                            <i class="fas fa-trash"></i>
+                                        </span>
+                                        <span class="text">Hapus</span>
+                                    </button>
+                                </td>
+                            </tr>
+                            @php
+                                $no++;
+                            @endphp
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        $(".btn-destroy").on('click', function(event) {
+            Swal.fire({
+                title: 'Hapus Data',
+                text: "Apakah anda yakin?",
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Hapus',
+                cancelButtonText: 'Batal',
+                customClass: {
+                    confirmButton: 'mr-2',
+                    cancelButton: 'ml-2'
+                }
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: 'Sedang Menghapus Data!',
+                        html: 'Mohon menunggu',
+                        timerProgressBar: true,
+                        allowOutsideClick: false,
+                        allowEscapeKey: false,
+                        didOpen: () => {
+                            Swal.showLoading()
+                        }
+                    })
+
+                    $.ajax({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        url: "{{ route('master.scout_level.destroy') }}/"+$(this).data('id'),
+                        type: "DELETE",
+                        success: function (res) {
+                            console.log(res);
+
+                            Swal.close()
+
+                            if (res.status) {
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: 'Sukses',
+                                    text: res.message,
+                                    allowOutsideClick: false,
+                                    allowEscapeKey: false
+                                }).then((result) => {
+                                    location.reload();
+                                })
+                            } else {
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Maaf',
+                                    text: res.message,
+                                    allowOutsideClick: false,
+                                    allowEscapeKey: false
+                                })
+                            }
+                        }
+                    });
+                }
+            })
+        });
+    </script>
+
+</div>
+@endsection
