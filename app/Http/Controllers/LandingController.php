326ba8d6771889040470b60c5b1a27bcbd48a6e8
DESKTOP-2ANV0GE\Fanky <andryfanky3@gmail.com>
2023-06-03 11:52:22 +0700

    feat: add statistic section in landing page

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\MstrKridaSakaMilenial;
 use App\Models\MstrKwarran;
 use App\Models\MstrScoutLevel;
+use App\Models\TrParticipants;
 use Illuminate\Http\Request;
 
 class LandingController extends Controller
@@ -15,10 +16,36 @@ class LandingController extends Controller
         $mstrScoutLevel = MstrScoutLevel::where('is_deleted',0)->get();
         $mstrKridaSakaMilenial = MstrKridaSakaMilenial::where('is_deleted',0)->get();
 
+        // * Statistic
+        $statistic = [];
+        $statisticCounter = 0;
+
+        $trParticipants = TrParticipants::with('mstrKwarran','mstrKridaSakaMilenial','trParticipantsRegistrationStatus')
+        ->orderBy('created_at','DESC')
+        ->get();
+
+        foreach ($trParticipants as $key => $value) {
+            $statistic[$statisticCounter]['no'] = $statisticCounter + 1;
+            $statistic[$statisticCounter]['full_name'] = $value->full_name;
+            $statistic[$statisticCounter]['krida_saka_milenial'] = $value->mstrKridaSakaMilenial->krida_saka_milenial;
+            $statistic[$statisticCounter]['kwarran'] = $value->mstrKwarran->kwarran;
+            if ($value->trParticipantsRegistrationStatus) {
+                if ($value->trParticipantsRegistrationStatus->status == 1) {
+                    $statistic[$statisticCounter]['status'] = '<span class="text-success"><b>Diterima</b></span>';
+                } else {
+                    $statistic[$statisticCounter]['status'] = '<span class="text-danger"><b>Ditolak</b></span>';
+                }
+            } else {
+                $statistic[$statisticCounter]['status'] = '<span class="text-warning"><b>Belum Diverifikasi</b></span>';
+            }
+            $statisticCounter++;
+        }
+
         return view('landing', [
             'kwarran' => $mstrKwarran,
             'scout_level' => $mstrScoutLevel,
-            'krida_saka_milenial' => $mstrKridaSakaMilenial
+            'krida_saka_milenial' => $mstrKridaSakaMilenial,
+            'statistic' => $statistic
         ]);
     }
 }
